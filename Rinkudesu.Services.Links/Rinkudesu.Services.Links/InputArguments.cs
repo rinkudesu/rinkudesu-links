64d403e74b61a24e0a2319cfe7bc409d371e4dac
Piotr Kowalski <piotr@pkowalski.pw>
2021-08-19 22:28:35 +0200

    Add missing whitespace after semicolon

--- Rinkudesu.Services.Links/Rinkudesu.Services.Links/InputArguments.cs
+++ Rinkudesu.Services.Links/Rinkudesu.Services.Links/InputArguments.cs
@@ -4,7 +4,7 @@ namespace Rinkudesu.Services.Links
 {
     public class InputArguments
     {
-        public static InputArguments Current { get; private set; } = null!;// the value is always set as the first operation of the program
+        public static InputArguments Current { get; private set; } = null!; // the value is always set as the first operation of the program
         [Option(longName: "applyMigrations", Required = false, HelpText = "Automatically creates the database and applies any missing migrations on startup")]
         public bool ApplyMigrations { get; set; }
 
