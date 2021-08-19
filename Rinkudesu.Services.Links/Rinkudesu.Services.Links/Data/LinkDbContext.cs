70b75dc859daca94d276fc57dc0f9289fe8b304c
Piotr Kowalski <piotr@pkowalski.pw>
2021-08-19 21:19:36 +0200

    Add basic link model and create initial database migration

--- Rinkudesu.Services.Links/Rinkudesu.Services.Links/Data/LinkDbContext.cs
+++ Rinkudesu.Services.Links/Rinkudesu.Services.Links/Data/LinkDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Rinkudesu.Services.Links.Models;
 
 namespace Rinkudesu.Services.Links.Data
 {
     public class LinkDbContext : DbContext
     {
+        public LinkDbContext(DbContextOptions<LinkDbContext> options) : base(options)
+        {
+        }
 
+        public DbSet<Link> Links => Set<Link>();
     }
 }
---
+++ Rinkudesu.Services.Links/Rinkudesu.Services.Links/Models/Link.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rinkudesu.Services.Links.Models
+{
+    public class Link
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [DataType(DataType.Url)]
+        [Required]
+        public string? LinkUrl { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [MaxLength(250)]
+        public string? Title { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string? Description { get; set; }
+        public LinkPrivacyOptions PrivacyOptions { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreationDate { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime LastUpdate { get; set; }
+        [Required]
+        public string CreatingUserId { get; set; }
+
+        public Link()
+        {
+            CreationDate = DateTime.Now;
+            LastUpdate = CreationDate;
+        }
+
+        public enum LinkPrivacyOptions
+        {
+            Private,
+            Public
+        }
+    }
+}
