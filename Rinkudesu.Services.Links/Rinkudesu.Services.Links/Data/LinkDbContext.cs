c1acb705c04c1e8219cd8c4a1779aa7103e41fa6
Piotr Kowalski <piotr@pkowalski.pw>
2021-08-19 21:01:39 +0200

    Add and initialise database context

---
+++ Rinkudesu.Services.Links/Rinkudesu.Services.Links/Data/LinkDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rinkudesu.Services.Links.Data
+{
+    public class LinkDbContext : DbContext
+    {
+
+    }
+}
---
+++ Rinkudesu.Services.Links/Rinkudesu.Services.Links/Utilities/EnvironmentalVariablesReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rinkudesu.Services.Links.Utilities
+{
+    public static class EnvironmentalVariablesReader
+    {
+        public const string DbContextVariableName = "RINKU_LINKS_CONNECTIONSTRING";
+        public static string GetRequiredVariable(string variableName)
+        {
+            var variableValue = Environment.GetEnvironmentVariable(variableName);
+            if (variableValue is null)
+            {
+                throw new InvalidOperationException($"Environmental variable {variableName} is not set");
+            }
+            return variableValue;
+        }
+    }
+}
