ee8d997e8a7b5fa255105a49092fee583b0c76f5
Piotr Kowalski <piotr@pkowalski.pw>
2021-08-25 20:44:04 +0200

    Add basic logging capabilities

--- Rinkudesu.Services.Links/Rinkudesu.Services.Links/InputArguments.cs
+++ Rinkudesu.Services.Links/Rinkudesu.Services.Links/InputArguments.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using CommandLine;
+using Serilog;
 
 namespace Rinkudesu.Services.Links
 {
@@ -9,10 +10,29 @@ namespace Rinkudesu.Services.Links
         public static InputArguments Current { get; private set; } = null!; // the value is always set as the first operation of the program
         [Option(longName: "applyMigrations", Required = false, HelpText = "Automatically creates the database and applies any missing migrations on startup")]
         public bool ApplyMigrations { get; set; }
+        [Option(longName: "muteConsoleLog", Required = false, HelpText = "Disables logging to console")]
+        public bool MuteConsoleLog { get; set; }
+        [Option(shortName: 'l', longName: "logLevel", Required = false, Default = 2, HelpText = "Defines log level: 0 - log everything, 5 - log errors only")]
+        public int LogLevel { get; set; }
+        [Option(longName: "logPath", Required = false, HelpText = "Optional path to a log file. Logs won't be saved to file if this is ignored.")]
+        public string? FileLogPath { get; set; }
 
         public void SaveAsCurrent()
         {
             Current = this;
+        }
+
+        public LoggerConfiguration GetMinimumLogLevel(LoggerConfiguration configuration)
+        {
+            return LogLevel switch
+            {
+                0 => configuration.MinimumLevel.Verbose(),
+                1 => configuration.MinimumLevel.Debug(),
+                2 => configuration.MinimumLevel.Information(),
+                3 => configuration.MinimumLevel.Warning(),
+                4 => configuration.MinimumLevel.Error(),
+                _ => configuration.MinimumLevel.Information()
+            };
         }
     }
 }
--- Rinkudesu.Services.Links/Rinkudesu.Services.Links/Program.cs
+++ Rinkudesu.Services.Links/Rinkudesu.Services.Links/Program.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using CommandLine;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 
 namespace Rinkudesu.Services.Links
 {
@@ -18,13 +22,47 @@ namespace Rinkudesu.Services.Links
             {
                 return 1;
             }
+            var logConfig = new LoggerConfiguration();
+            if (!InputArguments.Current.MuteConsoleLog)
+            {
+                logConfig.WriteTo.Console();
+            }
+            Log.Logger = logConfig.CreateBootstrapLogger();
 
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Application failed to start");
+                return 2;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
             return 0;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration) => {
+                    if (!InputArguments.Current.MuteConsoleLog)
+                    {
+                        configuration.WriteTo.Console();
+                    }
+                    if (InputArguments.Current.FileLogPath != null)
+                    {
+                        configuration.WriteTo.File(new RenderedCompactJsonFormatter(),
+                            InputArguments.Current.FileLogPath);
+                    }
+                    InputArguments.Current.GetMinimumLogLevel(configuration);
+                    configuration.ReadFrom.Services(services);
+                    configuration.Enrich.FromLogContext()
+                        .Enrich.WithProperty("ApplicationName", context.HostingEnvironment.ApplicationName)
+                        .Enrich.WithExceptionDetails();
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
