bc1bd37d8fd9a568eef2d96949ccd185e1c1700a
Piotr Kowalski <piotr@pkowalski.pw>
2022-01-06 16:27:38 +0100

    Add new exception type

---
+++ Rinkudesu.Services.Links/Rinkudesu.Services.Links.Repositories/Exceptions/DataInvalidException.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+
+namespace Rinkudesu.Services.Links.Repositories.Exceptions;
+
+[Serializable]
+[ExcludeFromCodeCoverage]
+public class DataInvalidException : RepositoryException
+{
+    public DataInvalidException() : base("No data matching the given conditions was found")
+    {
+    }
+
+    public DataInvalidException(string message) : base(message)
+    {
+    }
+
+    public DataInvalidException(Guid primaryKey) : base($"Data with primary key {primaryKey} was not invalid for the given context")
+    {
+    }
+
+    public DataInvalidException(Guid primaryKey, string reason) : base($"Data with primary key {primaryKey} was invalid for the following reason: {reason}")
+    {
+    }
+
+    protected DataInvalidException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
